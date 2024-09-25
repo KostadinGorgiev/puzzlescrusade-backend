e0d600cf685dc67f5cd497a2b41d67870f1d2edf
Kostadin <kostadingorgiev91@gmail.com>
2024-09-25 12:40:56 +0200

    update db config

--- src/database/sequelize.config.js
+++ src/database/sequelize.config.js
@@ -1,26 +1,26 @@
 module.exports = {
-  "development": {
-    "username": "root",
-    "password": "",
-    "database": "puzzlescrusade",
-    "host": "127.0.0.1",
-    "dialect": "mysql",
-    "logging": false
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: "puzzlescrusade",
+    host: "127.0.0.1",
+    dialect: "mysql",
+    logging: false,
   },
-  "test": {
-    "username": "root",
-    "password": "",
-    "database": "puzzlescrusade",
-    "host": "127.0.0.1",
-    "dialect": "mysql",
-    "logging": false
+  test: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: "puzzlescrusade",
+    host: "127.0.0.1",
+    dialect: "mysql",
+    logging: false,
   },
-  "production": {
-    "username": "root",
-    "password": "",
-    "database": "puzzlescrusade",
-    "host": "127.0.0.1",
-    "dialect": "mysql",
-    "logging": false
-  }
-}
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: "puzzlescrusade",
+    host: "127.0.0.1",
+    dialect: "mysql",
+    logging: false,
+  },
+};
